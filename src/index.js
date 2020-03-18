3d17284a4a08ca7c2a8998dd44c5824eee9272d4
Vitali <terrmit574@gmail.com>
2020-03-19 00:33:17 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  if (!matrix) return [];
+
+  let sortedMatrixArray = [];
+  // for(let i =0; i < matrix.length; i++) sortedMatrixArray = sortedMatrixArray.concat( matrix[i])
+  // console.log(sortedMatrixArray)
+  
+  for (let i = 0; i < matrix.length; i++) {
+    sortedMatrixArray = sortedMatrixArray.concat(i % 2 === 0 ? matrix[i].sort((a, b) => a - b) : matrix[i].sort((a, b) => b - a));
+  }
+  return sortedMatrixArray;
 }
